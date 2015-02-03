876e24f28eaa23c48db0f7a66b7d9640da1de199
David Stosik <dstosik@users.noreply.github.com>
2015-02-03 23:39:14 +0900

    Working symmetry
    Bonus: align center to Pebble Steel's apparent center (screen is offset because of the Pebble logo)

--- src/main.c
+++ src/main.c
@@ -12,15 +12,76 @@ static GBitmap *minutes_first_bitmap;
 static GBitmap *minutes_last_bitmap;
 static BitmapLayer *minutes_first_layer;
 static BitmapLayer *minutes_last_layer;
-static InverterLayer *inverter_layer;
+static Layer *symmetry_layer;
 
+static GPoint screen_center() {
+  GRect bounds = layer_get_bounds(window_get_root_layer(window));
+  switch(watch_info_get_model()) {
+    case WATCH_INFO_MODEL_PEBBLE_STEEL:
+      return GPoint(bounds.size.w/2, 95);
+    default:
+      return grect_center_point(&bounds);
+  }
+}
 
-// static void main_layer_update_proc(struct Layer *layer, GContext *ctx) {
-  
-  
+static GColor bg_color;
+static GColor fg_color;
+static GCompOp compositing;
+static void init_colors() {
+  bg_color = GColorBlack;
+  if (bg_color == GColorBlack) {
+    fg_color = GColorWhite;
+    compositing = GCompOpAssignInverted;
+  } else {
+    fg_color = GColorBlack;
+    compositing = GCompOpAssign;
+  }
+}
 
+static void symmetry_layer_update_proc(struct Layer *layer, GContext *ctx) {
   
-// }
+  GBitmap* frame_buffer = graphics_capture_frame_buffer(ctx);
+  uint8_t *image = frame_buffer->addr;
+  uint16_t lines = frame_buffer->bounds.size.h;
+  uint16_t cols = frame_buffer->bounds.size.w;
+
+  uint8_t * symmetry_data = malloc(lines * frame_buffer->row_size_bytes);
+  
+  uint8_t curr_byte = 0;
+  uint16_t line_byte_offset;
+  int16_t col_orig, col_sym;
+
+  for (uint16_t l = 0; l < lines; l++) {
+    line_byte_offset = l * frame_buffer->row_size_bytes;
+
+    for (col_sym = 0; col_sym < cols; col_sym++) {
+      col_orig = cols - col_sym - 1;
+
+      if(fg_color == ((image[line_byte_offset + col_orig/8] >> (col_orig%8)) & 1)) {
+        curr_byte += 1 << (col_sym%8);
+      }
+      if ((col_sym+1)%8 == 0) {
+        symmetry_data[line_byte_offset + col_sym/8] = curr_byte;
+        curr_byte = 0;
+      }
+    }
+    symmetry_data[line_byte_offset + col_sym/8] = curr_byte; // Last byte in a row
+  }
+  
+  graphics_release_frame_buffer(ctx, frame_buffer);
+  graphics_context_set_stroke_color(ctx, fg_color);
+
+  for (uint16_t l = 0; l < lines; l++) {
+    uint32_t line_byte_offset = l * frame_buffer->row_size_bytes;
+    for (uint16_t c = 0; c < cols; c++) {
+      uint32_t total_byte_offset = line_byte_offset + c/8;
+      if ((symmetry_data[total_byte_offset] >> (c%8)) & 1) {
+        graphics_draw_pixel(ctx, GPoint(c, l));
+      }
+    }
+  }
+  free(symmetry_data);
+}
 
 static void update_time() {
   if(hours_first_bitmap) gbitmap_destroy(hours_first_bitmap);
@@ -43,54 +104,56 @@ static void update_time() {
   bitmap_layer_set_bitmap(minutes_last_layer, minutes_last_bitmap);
   layer_mark_dirty(bitmap_layer_get_layer(minutes_last_layer));
 
-//   layer_mark_dirty(main_layer);
+  layer_mark_dirty(symmetry_layer);
 }
 
 static void window_load(Window *window) {
+  init_colors();
+  
+  window_set_background_color(window, bg_color);
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
   
+  GPoint center = screen_center();
   main_layer = layer_create(GRect(
-    (bounds.size.w - MAIN_LAYER_WIDTH) / 2,
-    (bounds.size.h - MAIN_LAYER_HEIGHT) / 2,
+    center.x - MAIN_LAYER_WIDTH/2,
+    center.y - MAIN_LAYER_HEIGHT/2,
     MAIN_LAYER_WIDTH,
     MAIN_LAYER_HEIGHT
   ));
   layer_add_child(window_layer, main_layer);
 
-//   layer_set_update_proc(main_layer, main_layer_update_proc);
-  
-  hours_first_bitmap = gbitmap_create_with_resource(hours_first_digit(NULL));
   hours_first_layer = bitmap_layer_create(GRect(
     HOURS_FIRST_X, HOURS_FIRST_Y,
     HOURS_FIRST_WIDTH, HOURS_FIRST_HEIGHT
   ));
+  bitmap_layer_set_compositing_mode(hours_first_layer, compositing);
   layer_add_child(main_layer, bitmap_layer_get_layer(hours_first_layer));
-  
-  hours_last_bitmap = gbitmap_create_with_resource(hours_last_digit(NULL));
+
   hours_last_layer = bitmap_layer_create(GRect(
     HOURS_LAST_X, HOURS_LAST_Y,
     HOURS_LAST_WIDTH, HOURS_LAST_HEIGHT
   ));
+  bitmap_layer_set_compositing_mode(hours_last_layer, compositing);
   layer_add_child(main_layer, bitmap_layer_get_layer(hours_last_layer));
-  
-  minutes_first_bitmap = gbitmap_create_with_resource(minutes_first_digit(NULL));
+
   minutes_first_layer = bitmap_layer_create(GRect(
     MINUTES_FIRST_X, MINUTES_FIRST_Y,
     MINUTES_FIRST_WIDTH, MINUTES_FIRST_HEIGHT
   ));
+  bitmap_layer_set_compositing_mode(minutes_first_layer, compositing);
   layer_add_child(main_layer, bitmap_layer_get_layer(minutes_first_layer));
-  
-  minutes_last_bitmap = gbitmap_create_with_resource(minutes_last_digit(NULL));
+
   minutes_last_layer = bitmap_layer_create(GRect(
     MINUTES_LAST_X, MINUTES_LAST_Y,
     MINUTES_LAST_WIDTH, MINUTES_LAST_HEIGHT
   ));
+  bitmap_layer_set_compositing_mode(minutes_last_layer, compositing);
   layer_add_child(main_layer, bitmap_layer_get_layer(minutes_last_layer));
 
-  
-  inverter_layer = inverter_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
-  layer_add_child(window_layer, inverter_layer_get_layer(inverter_layer));
+  symmetry_layer = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  layer_add_child(window_layer, symmetry_layer);
+  layer_set_update_proc(symmetry_layer, symmetry_layer_update_proc);
   
   update_time();
 }
@@ -105,7 +168,7 @@ static void window_unload(Window *window) {
   bitmap_layer_destroy(hours_last_layer);
   bitmap_layer_destroy(minutes_first_layer);
   bitmap_layer_destroy(minutes_last_layer);
-  inverter_layer_destroy(inverter_layer);
+  layer_destroy(symmetry_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -130,9 +193,6 @@ static void deinit(void) {
 
 int main(void) {
   init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
   app_event_loop();
   deinit();
 }
