f2cb6104cbc04407ca51c1c73c3368cf48e40084
David Stosik <david.stosik+git-noreply@gmail.com>
2015-02-07 15:55:25 +0900

    Enable live_reload in dev.

--- gh-pages/config.rb
+++ gh-pages/config.rb
@@ -40,9 +40,9 @@ require 'bootstrap-sass'
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+ activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
