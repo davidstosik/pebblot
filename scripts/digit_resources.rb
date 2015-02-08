418ebaebc83409f8f619bac6f91d70bdd0868ed0
David Stosik <david.stosik+git-noreply@gmail.com>
2015-02-08 22:31:11 +0900

    Prepare part of the image resources to melt digits together.

--- scripts/digit_resources.rb
+++ scripts/digit_resources.rb
@@ -2,9 +2,9 @@
 
 require 'json'
 
-base_path = 'images/digits'
 type = 'png'
 
+base_path = 'images/digits'
 files =
   (0..3).map do |position|
     (0..10).map do |digit|
@@ -18,4 +18,19 @@ files =
     end
   end.flatten.compact
 
-puts JSON.pretty_generate files
+sides = %w(top bottom left right)
+part = sides + %w(center)
+base_path = 'images/melted'
+files += (0..3).map do |position|
+  Dir["./resources/#{base_path}/#{position}/*.#{type}"].map do |file|
+    basename = File.basename file
+    {
+      file: "#{base_path}/#{position}/#{basename}",
+      name: 'MELTED_' + basename.sub(/\.#{type}$/, '').upcase[1..-1],
+      type: type,
+    }
+  end.flatten.compact
+
+end
+
+puts JSON.pretty_generate files.flatten
