557595e9338fea3b4d15c2e447c14a0e202cc594
David Stosik <david.stosik+git-noreply@gmail.com>
2015-02-09 18:52:40 +0900

    Automated commit at 2015-02-09 09:52:40 UTC by middleman-deploy 1.0.0

--- js/config.js
+++ js/config.js
@@ -1,43 +1,54 @@
 (function() {
   $(function() {
-    var event_props, settings, uid, _ref;
+    var event_props, settings_hash, uid, _ref;
     try {
-      settings = JSON.parse(decodeURIComponent($.urlParam('settings')));
+      settings_hash = JSON.parse(decodeURIComponent($.urlParam('settings')));
     } catch (_error) {
-      settings = {};
+      settings_hash = {};
     }
-    if (typeof settings !== 'object') {
-      settings = {};
+    if (typeof settings_hash !== 'object') {
+      settings_hash = {};
     }
-    $('.debug').append("Received settings: " + JSON.stringify(settings) + "\n");
+    $('.debug').append("Received settings: " + JSON.stringify(settings_hash) + "\n");
     if ($.urlParam('debug')) {
       $('.debug').removeClass('hidden');
       $('input#submit').addClass('btn-danger');
     }
-    $.each(settings, function(name, value) {
+    $.each(settings_hash, function(name, value) {
       var element;
       element = $("[name=" + name + "]");
-      if (element.attr('type') === 'radio') {
-        return element.filter("[value=" + value + "]").click();
+      if (element.prop('tagName') === 'INPUT') {
+        switch (element.attr('type')) {
+          case 'radio':
+            element.filter("[value=" + value + "]").click();
+            break;
+          case 'checkbox':
+            return element.prop('checked', value);
+        }
       } else {
         return element.val(value);
       }
     });
     $('form#settings').submit(function(event) {
       var location;
-      settings = {};
-      $.each($('form').serializeArray(), function() {
-        return settings[this.name] = parseInt(this.value);
+      settings_hash = {};
+      $('form#settings select').each(function() {
+        settings_hash[$(this).prop('name')] = parseInt($(this).val());
+        return true;
       });
-      $('.debug').append("Prepared settings: " + JSON.stringify(settings) + "\n");
-      mixpanel.people.set(settings);
-      location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(settings));
+      $('form#settings input[type=checkbox]').each(function() {
+        settings_hash[$(this).prop('name')] = $(this).is(':checked');
+        return true;
+      });
+      $('.debug').append("Prepared settings: " + JSON.stringify(settings_hash) + "\n");
+      mixpanel.people.set(settings_hash);
+      location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(settings_hash));
       $('.debug').append("Returned URL: " + location + "\n");
       if ($('input#submit').hasClass('btn-danger')) {
         $('.debug').append("Click again!\n");
         $('input#submit').removeClass('btn-danger');
       } else {
-        mixpanel.track('Submitted settings form', settings);
+        mixpanel.track('Submitted settings form', settings_hash);
         document.location = location;
       }
       return event.preventDefault();
@@ -46,9 +57,9 @@
       ga('set', '&uid', uid);
       mixpanel.identify(uid);
     }
-    event_props = ((_ref = settings === {}) != null ? _ref : {
+    event_props = ((_ref = settings_hash === {}) != null ? _ref : {
       empty_settings: true
-    }) || settings;
+    }) || settings_hash;
     event_props['debug'] = $.urlParam('debug');
     event_props['page'] = 'config';
     mixpanel.track('Page', event_props);
